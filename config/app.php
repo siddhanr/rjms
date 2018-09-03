8695eddc78a7bba5d5f364afda9fbef156c28a46
Siddha Rusdi <siddhanrusdi@gmail.com>
2018-09-03 23:27:22 +1200

    fixed html class not found

--- config/app.php
+++ config/app.php
@@ -136,7 +136,6 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-        Illuminate\Html\HtmlServiceProvider::class,
 
 
         /*
@@ -195,8 +194,6 @@ return [
         'URL'       => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
-        'Form'      => Illuminate\Html\FormFacade::class,
-        'Html'      => Illuminate\Html\HtmlFacade::class,
 
     ],
 
